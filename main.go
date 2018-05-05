d5880a5256853146b357579a952fe24c1c7bcbf5
c33483458 <joyal.joy@standardbank.co.za>
2018-05-05 22:12:50 +0200

    service for load branch cache for easytrace

--- main.go
+++ main.go
@@ -4,16 +4,21 @@ import (
 	"fmt"
 	"github.com/go-kit/kit/log"
 	"github.com/go-kit/kit/log/level"
-	/*"github.com/weAutomateEverything/go2hal/remoteTelegramCommands"
+	"github.com/weAutomateEverything/go2hal/remoteTelegramCommands"
 	kitprometheus "github.com/go-kit/kit/metrics/prometheus"
-	stdprometheus "github.com/prometheus/client_golang/prometheus"*/
+	stdprometheus "github.com/prometheus/client_golang/prometheus"
 	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
-	/*"github.com/weAutomateEverything/go2hal/alert"
-	"github.com/weAutomateEverything/easytraceCache"*/
+	"github.com/weAutomateEverything/easytraceCache"
+	"github.com/weAutomateEverything/go2hal/alert"
+	"github.com/weAutomateEverything/go2hal/database"
+
+	"github.com/weAutomateEverything/go2hal/chef"
+	"github.com/weAutomateEverything/go2hal/telegram"
+
 )
 
 func main() {
@@ -22,11 +27,27 @@ func main() {
 	logger = log.NewLogfmtLogger(log.NewSyncWriter(os.Stderr))
 	logger = level.NewFilter(logger, level.AllowAll())
 	logger = log.With(logger, "ts", log.DefaultTimestamp)
-	/*fieldKeys := []string{"method"}
+	fieldKeys := []string{"method"}
+	db := database.NewConnection()
+	chefStore := chef.NewMongoStore(db)
+	telegramStore := telegram.NewMongoStore(db)
 	remoteTelegramService := remoteTelegramCommands.NewRemoteCommandClientService()
 	alertService := alert.NewKubernetesAlertProxy("")
-
-	cacheService := easytraceCache.NewService(remoteTelegramService, alertService)
+	chefService := chef.NewService(alertService, chefStore)
+	chefService = chef.NewLoggingService(log.With(logger, "component", "chef"), chefService)
+	chefService = chef.NewInstrumentService(kitprometheus.NewCounterFrom(stdprometheus.CounterOpts{
+		Namespace: "api",
+		Subsystem: "chef",
+		Name:      "request_count",
+		Help:      "Number of requests received.",
+	}, fieldKeys),
+		kitprometheus.NewSummaryFrom(stdprometheus.SummaryOpts{
+			Namespace: "api",
+			Subsystem: "chef",
+			Name:      "request_latency_microseconds",
+			Help:      "Total duration of requests in microseconds.",
+		}, fieldKeys), chefService)
+	cacheService := easytraceCache.NewService(remoteTelegramService, alertService,chefService,chefStore,telegramStore)
 	cacheService = easytraceCache.NewLoggingService(log.With(logger, "component", "easytraceCache"), cacheService)
 	cacheService = easytraceCache.NewInstrumentService(kitprometheus.NewCounterFrom(stdprometheus.CounterOpts{
 		Namespace: "api",
@@ -40,7 +61,7 @@ func main() {
 			Name:      "request_latency_microseconds",
 			Help:      "Total duration of requests in microseconds.",
 		}, fieldKeys), cacheService)
-*/
+
 	http.Handle("/metrics", promhttp.Handler())
 
 	errs := make(chan error, 2)
